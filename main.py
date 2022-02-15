b7f5949922d8d5b53f8ddb0e57ba69f09043a893
Zachary Smith <22395357+Cutwell@users.noreply.github.com>
2022-02-15 13:10:50 +0000

    Update main.py
    - Optimized script accessing scene sources
    - Implemented better scene accessing script
    - Added dropdown lists for setting avatar sources

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ global interval
 global threshold
 global avatar_speaking
 global avatar_mute
+global previous_state
 
 global activation
 global activation_hold
@@ -15,6 +16,7 @@ interval        = 100   #ms
 threshold       = 25
 avatar_speaking = ""
 avatar_mute     = ""
+previous_state  = True  # initial state is (volume_norm < threshold) == True
 
 activation          = False
 activation_hold     = 500
@@ -25,9 +27,10 @@ def update(indata, outdata, frames, time, status):
     global avatar_speaking
     global avatar_mute
     global activation
+    global previous_state
 
     volume_norm = np.linalg.norm(indata)*10
-    print(int(volume_norm))
+    #print(int(volume_norm))    # active activation threshold logger
 
     # skip update if animating
     if activation:
@@ -41,30 +44,38 @@ def update(indata, outdata, frames, time, status):
             activation = False
             activation_clock = 0
 
-    else:
-        current_scene   = obspython.obs_scene_from_source(obspython.obs_frontend_get_current_scene())
-        speaking_source = obspython.obs_scene_find_source(current_scene, avatar_speaking)
-        mute_source     = obspython.obs_scene_find_source(current_scene, avatar_mute)
+    # access sources only on state change
+    elif previous_state != (volume_norm < threshold):
+        previous_state = (volume_norm < threshold)
 
-        if current_scene and speaking_source and mute_source:
+        if volume_norm < threshold:
+            speaking_toggle, mute_toggle = False, True
+        else:
+            speaking_toggle, mute_toggle = True, False
+            # activate animation hold
+            activation = True
 
-            if volume_norm < threshold:
-                # show mute, hide speaking
-                obspython.obs_sceneitem_set_visible(mute_source, True)
-                obspython.obs_sceneitem_set_visible(speaking_source, False)
-                
-            else:
-                # show speaking, hide mute
-                obspython.obs_sceneitem_set_visible(speaking_source, True)
-                obspython.obs_sceneitem_set_visible(mute_source, False)
+        scene_sources = obspython.obs_frontend_get_scenes()
 
-                # activate animation hold
-                activation = True
+        for scn_src in scene_sources:
 
-            # release sources after updating
-            obspython.obs_scene_release(current_scene)
-        
-	
+            scn = obspython.obs_scene_from_source(scn_src)
+            scn_items = obspython.obs_scene_enum_items(scn)
+
+            for itm in scn_items:
+
+                itm_src = obspython.obs_sceneitem_get_source(itm)
+
+                if obspython.obs_source_get_name(itm_src) == avatar_speaking:
+                    obspython.obs_sceneitem_set_visible(itm, speaking_toggle)
+
+                elif obspython.obs_source_get_name(itm_src) == avatar_mute:
+                    obspython.obs_sceneitem_set_visible(itm, mute_toggle)
+
+            obspython.sceneitem_list_release(scn_items)
+
+        obspython.source_list_release(scene_sources)
+
 def audio_read():
     global interval
 
@@ -75,11 +86,14 @@ def audio_read():
 def refresh_pressed(props, prop):
     global activation
     global activation_clock
+    global interval
+
+    obspython.timer_remove(audio_read)
 
     activation          = False
     activation_clock    = 0
 
-    audio_read()
+    obspython.timer_add(audio_read, interval)
 
 def script_description():
 	return "Monitors microphone audio input to display appropriate avatar GIFs.\n\nBy Cutwell"
@@ -91,27 +105,48 @@ def script_update(settings):
     global avatar_mute
     global activation_hold
 
-    threshold       = obspython.obs_data_get_int(settings, "threshold")
-    activation_hold = obspython.obs_data_get_int(settings, "activation_hold")
-    avatar_speaking = obspython.obs_data_get_string(settings, "avatar_speaking")
-    avatar_mute     = obspython.obs_data_get_string(settings, "avatar_mute")
-
     obspython.timer_remove(audio_read)
 
-    if threshold != "" and activation_hold != "" and avatar_speaking != "" and avatar_mute != "":
+    interval        = obspython.obs_data_get_int(settings, "interval")
+    threshold       = obspython.obs_data_get_int(settings, "threshold")
+    activation_hold = obspython.obs_data_get_int(settings, "activation_hold")
+    avatar_speaking = obspython.obs_data_get_string(settings, "speaking_source_select_list")
+    avatar_mute     = obspython.obs_data_get_string(settings, "mute_source_select_list")
+
+    if interval != "" and threshold != "" and activation_hold != "" and avatar_speaking != "" and avatar_mute != "":
 	    obspython.timer_add(audio_read, interval)
 
 def script_defaults(settings):
+    obspython.obs_data_set_default_int(settings, "interval", 100)
     obspython.obs_data_set_default_int(settings, "threshold", 25)
     obspython.obs_data_set_default_int(settings, "activation_hold", 2000)
 
 def script_properties():
     props = obspython.obs_properties_create()
-
+    
+    obspython.obs_properties_add_int(props, "interval", "Animation hold interval", 5, 3600, 1)
     obspython.obs_properties_add_int(props, "threshold", "Activation threshold (normalised volume)", 5, 3600, 1)
     obspython.obs_properties_add_int(props, "activation_hold", "Activation hold (ms)", 5, 3600, 1)
-    obspython.obs_properties_add_text(props, "avatar_speaking", "Avatar speaking Source", obspython.OBS_TEXT_DEFAULT)
-    obspython.obs_properties_add_text(props, "avatar_mute", "Avatar mute Source", obspython.OBS_TEXT_DEFAULT)
+    
+    avatar_speaking_drop_list = obspython.obs_properties_add_list(props, "speaking_source_select_list", "Avatar speaking source", obspython.OBS_COMBO_TYPE_LIST, obspython.OBS_COMBO_FORMAT_STRING)
+    avatar_mute_drop_list = obspython.obs_properties_add_list(props, "mute_source_select_list", "Avatar mute source", obspython.OBS_COMBO_TYPE_LIST, obspython.OBS_COMBO_FORMAT_STRING)
+    
+    obspython.obs_property_list_add_string(avatar_speaking_drop_list, "", "")
+    obspython.obs_property_list_add_string(avatar_mute_drop_list, "", "")
+
+    # append sources to drop lists
+    sources = obspython.obs_enum_sources()
+    for src in sources:
+        obspython.obs_property_list_add_string(avatar_speaking_drop_list, obspython.obs_source_get_name(src), obspython.obs_source_get_name(src))
+        obspython.obs_property_list_add_string(avatar_mute_drop_list, obspython.obs_source_get_name(src), obspython.obs_source_get_name(src))
+    obspython.source_list_release(sources)
+    
     obspython.obs_properties_add_button(props, "button", "Refresh", refresh_pressed)
 	
     return props
+
+def script_save(settings):
+    obspython.obs_save_sources()
+
+def script_unload():
+    obspython.timer_remove(audio_read)
